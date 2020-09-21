8973df0576f58f08ef61e4b1f4663ba4944e1cd8
Sayan-Mitra-007 <sayanmitra098@gmail.com>
2020-09-22 01:01:28 +0530

    Commited

--- src/app/add-flight/add-flight.component.ts
+++ src/app/add-flight/add-flight.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import {addFlight} from '../add-flight/addFlight';
 @Component({
   selector: 'app-add-flight',
   templateUrl: './add-flight.component.html',
@@ -12,6 +12,11 @@ export class AddFlightComponent implements OnInit {
   constructor(
     private fb:FormBuilder
   ) { }
+  
+  af:addFlight=new addFlight();
+  Fromcities=["Hyderabad","Chennai","Mumbai","New Delhi","Banglore"];
+  Tocities=["Hyderabad","Chennai","Mumbai","New Delhi","Banglore"];
+  weekDay=["Sunday","Monday","Tueaday","Wednesday","Thrusday","Friday","Saturday"];
 
   ngOnInit(): void {
     this.buildAddFlightForm();
@@ -31,4 +36,15 @@ export class AddFlightComponent implements OnInit {
     })
   }
 
+  Fromcity(e)
+  {
+    this.Tocities=["Hyderabad","Chennai","Mumbai","New Delhi","Banglore"];
+    const index:number = this.Tocities.indexOf(e.target.value);
+    this.Tocities.splice(index,1);
+  }
+
+  submit()
+  {
+    alert(JSON.stringify(this.af));
+  }
 }
