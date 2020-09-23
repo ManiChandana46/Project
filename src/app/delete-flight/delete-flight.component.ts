cb6c7c100689adda12e27770da15f6c23b758b1d
Sayan-Mitra-007 <sayanmitra098@gmail.com>
2020-09-24 02:27:47 +0530

    Delete Flight & Dashboard done

--- src/app/delete-flight/delete-flight.component.ts
+++ src/app/delete-flight/delete-flight.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {deleteFlight} from '../delete-Flight/deleteFlight';
 @Component({
   selector: 'app-delete-flight',
   templateUrl: './delete-flight.component.html',
   styleUrls: ['./delete-flight.component.css']
 })
 export class DeleteFlightComponent implements OnInit {
+  deleteFlightForm:FormGroup;
+  constructor(
+    private fb:FormBuilder
+  ) { }
 
-  constructor() { }
+  df: deleteFlight=new deleteFlight();
 
   ngOnInit(): void {
+    this.builddeleteFlightForm();
+  }
+
+  builddeleteFlightForm() :void
+  {
+    this.deleteFlightForm=this.fb.group({
+      flightNumber:['',Validators.required]
+    })
+  }
+
+  submit()
+  {
+    if(this.deleteFlightForm.valid)
+    {
+      alert(JSON.stringify(this.df));
+    }
+    else{
+      alert('Please enter all the fields.')
+    }
+    
   }
 
 }
---
+++ src/app/delete-flight/deleteFlight.ts
@@ -0,0 +1,4 @@
+export class deleteFlight
+{
+    public flightNumber:number;
+}
