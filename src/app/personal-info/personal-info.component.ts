e372d0ac4cf9f8f44e75e752bababc4d8e0a9da3
anshul <anshsharma4650@gmail.com>
2020-09-26 01:26:12 +0530

    Added Validation in PaymentInfo and Done CSS changes in payment page

--- src/app/personal-info/personal-info.component.ts
+++ src/app/personal-info/personal-info.component.ts
@@ -13,30 +13,57 @@ export class PersonalInfoComponent implements OnInit {
   gender: string;
   list: Passenger[];
 
+  noOfPassenger: number;
+  limit: number;
+  myItem: any;
+
   ngOnInit() {
     this.list = [];
     this.name = '';
     this.age = '';
     this.gender = 'Male';
+    this.noOfPassenger = 0;
+    this.myItem=JSON.parse(sessionStorage.getItem("searchDetails"));
+    this.limit = this.myItem.noOfPassengers;
+    (<HTMLInputElement>document.getElementById('continue')).disabled = true;
   }
 
+
   addPassenger() {
-    if (this.name !== '' && this.age !== '' && this.gender !== '') {
+
+    let addedPassenger = this.list.find(o => o.name.toLowerCase() === this.name.toLowerCase());
+
+    if (this.name !== '' && this.age !== '' && this.gender !== '' && addedPassenger === undefined) {
       const newItem: Passenger = {
         name: this.name,
         age: this.age,
         gender: this.gender,
       };
       this.list.push(newItem);
-
+      this.noOfPassenger++;
       this.name = '';
       this.age = '';
       this.gender = 'Male';
+    }
+
+    else if (this.name === '' || this.age === '')
+      alert("Please fill all the required feilds");
+
+    else {
+      alert(this.name + " already added");
+    }
+
+    if (this.noOfPassenger === this.limit) {
+      (<HTMLInputElement>document.getElementById('addPass')).disabled = true;
+      (<HTMLInputElement>document.getElementById('continue')).disabled = false;
     }
   }
 
   deleteName(index: number) {
     this.list.splice(index, 1);
+    this.noOfPassenger--;
+    (<HTMLInputElement>document.getElementById('addPass')).disabled = false;
+    (<HTMLInputElement>document.getElementById('continue')).disabled = true;
   }
 
 }
