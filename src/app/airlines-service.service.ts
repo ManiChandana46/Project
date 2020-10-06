d06cb7c86299a484e21361a7bb7814029f761767
anshul <anshsharma4650@gmail.com>
2020-10-06 17:26:27 +0530

    Booking end to end done

--- src/app/airlines-service.service.ts
+++ src/app/airlines-service.service.ts
@@ -1,38 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { User } from './user';
 import { RegisterStatus } from './registerStatus';
 import { Observable } from 'rxjs';
 import { BookedTiketSearch } from './booking-details/bookedTicketSearch';
-import { Booking } from './booking-details/bookings';
-import { CheckRegistration } from './checkResgitration'
-import { CancelBooking } from './cancelBooking';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AirlinesServiceService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
+  register(user: User): Observable<RegisterStatus> {
+    let url = 'http://localhost:8181/addregistration';
+    return this.http.post<RegisterStatus>(url, user);
+  }
 
-  register(user:User):Observable<RegisterStatus>{
-    let url="http://localhost:8181/addregistration";
-    return this.http.post<RegisterStatus>(url,user);
+  bookedTicketSearch(ticketSearch: BookedTiketSearch): Observable<any> {
+    let url = 'http://localhost:8181/searchbooking';
+    return this.http.post(url, ticketSearch);
   }
-  /*check(user:User):Observable<any>{
-    let url="http://localhost:8181/checkregistration";
-    return this.http.post<any>(url,user.customerEmail);
-  }*/
+  cancelBooking(cancelticket: BookedTiketSearch): Observable<any> {
+    let url = 'http://localhost:8181/cancelbooking';
+    return this.http.post(url, cancelticket);
+  }
+  cancelReturnBooking(cancelticket: BookedTiketSearch): Observable<any> {
+    let url = 'http://localhost:8181/cancelreturnbooking';
+    return this.http.post(url, cancelticket);
+  }
 
-  bookedTicketSearch(ticketSearch:BookedTiketSearch):Observable<any>{
-    let url="http://localhost:8181/searchbooking";
-    return this.http.post<Booking>(url,ticketSearch);
+  displayBooking(customerId: number):Observable<any> {
+    let url = 'http://localhost:8181/displayBookingDetails?customerId=' + customerId;
+    return this.http.get(url)
   }
-  cancelBooking(cancelticket:BookedTiketSearch):Observable<any>{
-    let url="http://localhost:8181/cancelbooking";
-    return this.http.post<any>(url,cancelticket);
-  }
-  cancelReturnBooking(cancelticket:CancelBooking):Observable<any>{
-    let url="http://localhost:8181/cancelbooking";
-    return this.http.post<any>(url,cancelticket);
-  }
+
+
 }
--- src/app/booking-details/booking-details.component.ts
+++ src/app/booking-details/booking-details.component.ts
@@ -4,140 +4,119 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BookedTiketSearch } from '../booking-details/bookedTicketSearch';
 import { AirlinesServiceService } from '../airlines-service.service';
-import { CancelBooking } from '../cancelBooking'
+import { NgxSpinnerService } from 'ngx-spinner';
+
 @Component({
   selector: 'app-booking-details',
   templateUrl: './booking-details.component.html',
   styleUrls: ['./booking-details.component.css'],
 })
 export class BookingDetailsComponent implements OnInit {
-  booking: Booking;
-  bookings: Booking[]=[];
-  book: Booking;
+  bookings: Booking[];
   SearchTicketForm: FormGroup;
-  submitted = false;
   show = false;
   return = false;
- 
-  message="";
-  disabled=false;
+
+  customerId:number;
+
+  message = '';
+  disabled = false;
   show1 = false;
 
   get TicketId() {
     return this.SearchTicketForm.get('ticketId');
   }
 
-  constructor(private modalService: NgbModal, private fb: FormBuilder,private service: AirlinesServiceService) { }
+  constructor(
+    private modalService: NgbModal,
+    private fb: FormBuilder,
+    private service: AirlinesServiceService,
+    private spinner: NgxSpinnerService
+  ) {}
 
   ngOnInit(): void {
-    if (sessionStorage.getItem("login") == "false")
-    {
-      this.message="*You have to login first";
-      this.disabled=true;
-    }
-    else{
-      this.message="";
-      this.disabled=false;
+    this.spinner.show();
+    this.bookings = [];
+
+    if (sessionStorage.getItem('login') == 'false') {
+      this.message = '*You have to login first';
+      this.disabled = true;
+    } else {
+      this.message = '';
+      this.disabled = false;
     }
 
-    this.bookings = [
-      new Booking(
-        601,
-        '23-10-2020',
-        2,
-        '2A,2B',
-        'bangalore',
-        'chennai',
-        '11:00',
-        '12:00',
-        'economy',
-        500,
-        2,
-        'buissness',
-        '3A,3B',
-        'chennai',
-        'bangalore',
-        '15:00',
-        '17:00'),
-    ];
     this.SearchTicketForm = this.fb.group({
-
       //TicketId Validator
-      ticketId: ['', [Validators.required, Validators.minLength(1)]]
+      ticketId: ['', [Validators.required, Validators.minLength(1)]],
+    });
 
+
+    this.customerId = Number(sessionStorage.getItem("customerId"));
+
+    this.service.displayBooking(this.customerId).subscribe((response) => {
+      this.bookings = response;
+      this.show = true;
+      this.show1 = true;
+      this.spinner.hide();
     });
   }
-  info:string;
-  bookingId:number;
+  info: string;
+  bookingId: number;
   search: BookedTiketSearch = new BookedTiketSearch();
   searchTicket() {
     if (this.SearchTicketForm.valid) {
       this.show = true;
-      alert(JSON.stringify(this.search))
-      /*if (this.re == 1) {
-        this.return = true;
 
-      }*/
-      
-      /*this.service.bookedTicketSearch(this.search).subscribe((response)=>{
-        this.bookings=response;
-        if(this.bookings.returnId!=0){
+      this.service.bookedTicketSearch(this.search).subscribe((response) => {
+        let addedBooking = this.bookings.find((o) => o.bookId === response.bookId);
+
+        if (response.returnId != 0) {
+          if (addedBooking === undefined) this.bookings.push(response);
           this.show = true;
-          this.show1=true;
-          this.bookingId=this.bookings.bookId;
-
-          
-        }
-        else{
-          this.bookings=response;
+          this.show1 = true;
+          this.bookingId = response.bookId;
+        } else {
+          if (addedBooking === undefined) this.bookings.push(response);
           this.show = true;
         }
-        
-      })*/
-    }
-    else {
-      alert("Enter TicketId")
+      });
+    } else {
+      alert('Enter TicketId');
     }
   }
-  cancelBooking(){
+  cancelBooking() {
     //sessionStorage.setItem("bookingId",)
   }
-  cancelReturn(){
-
-  }
-  
+  cancelReturn() {}
 
   open(content: any, id: number) {
     this.modalService
       .open(content, { ariaLabelledBy: 'modal-basic-title' })
       .result.then((result) => {
-        //if (`${result}` === 'Save click') this.bookings.splice(id, 1);
-        this.service.cancelBooking(this.search).subscribe((response)=>{
-          this.info=response;
-          this.show = false;
-        });
-
-
+        if (`${result}` === 'Save click') {
+          this.service.cancelBooking(this.search).subscribe((response) => {
+            this.bookings.splice(id, 1);
+            //this.info = response;
+            //this.show = false;
+          });
+        }
       });
   }
-  cancel:CancelBooking=new CancelBooking
+
   open1(content: any, id: number) {
     this.modalService
       .open(content, { ariaLabelledBy: 'modal-basic-title' })
       .result.then((result) => {
-        //if (`${result}` === 'Save click') this.bookings.splice(id, 1);
-        this.service.cancelReturnBooking(this.cancel).subscribe((response)=>{
-          this.info=response;
-          this.show = false;
-        });
-
-
+        if (`${result}` === 'Save click') {
+          this.service
+            .cancelReturnBooking(this.search)
+            .subscribe((response) => {
+              //this.bookings.splice(id, 1);
+              // this.info = response;
+              this.show1 = false;
+            });
+        }
       });
   }
-
-
-  // cancel() {
-
-  //   sessionStorage.setItem("bookingDetails",JSON.stringify(this.book));
-  // }
 }
