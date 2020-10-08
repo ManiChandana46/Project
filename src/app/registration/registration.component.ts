12dcd24541a7a16af221bec611314dc692c7921e
Harshit-h119 <guptaharshit13@gmail.com>
2020-10-08 14:47:40 +0530

    indentation done

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ForbiddenNameValidator } from '../../assets/shared/user-name.validator';
 import { PasswordValidator } from '../../assets/shared/password.validator';
 import { ContactNumberValidator } from '../../assets/shared/contact-number.validator';
-import{ User} from '../user';
+import { User } from '../user';
 import { Router } from '@angular/router';
 
 import { HttpClient } from '@angular/common/http';
@@ -20,124 +20,100 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  title='Register Page';
+  title = 'Register Page';
   //getter method for first name.
-  user:User=new User();
+  user: User = new User();
 
-  get FName(){
+  get FName() {
     return this.registerForm.get('fName');
   }
 
   //getter method for Last name.
-  get LName(){
+  get LName() {
     return this.registerForm.get('lName');
   }
 
   //getter method for Email.
-  get Email(){
+  get Email() {
     return this.registerForm.get('email');
   }
 
   //getter method for contact Number.
-  get ContactNumber(){
+  get ContactNumber() {
     return this.registerForm.get('contactNumber');
   }
 
 
 
   //constructor
-  constructor(private fb: FormBuilder,private router:Router,private service: AirlinesServiceService , private http: HttpClient, private spinner: NgxSpinnerService){}
+  constructor(private fb: FormBuilder, private router: Router, private service: AirlinesServiceService, private http: HttpClient, private spinner: NgxSpinnerService) { }
 
 
 
   //declare variable of form group type.
   registerForm: FormGroup;
-  submitted= false;
+  submitted = false;
 
 
 
   //Oninit method.
-  ngOnInit(){
-    this.registerForm= this.fb.group({
+  ngOnInit() {
+    this.registerForm = this.fb.group({
       //First Name Validator
-      fName: ['',[Validators.required, Validators.minLength(3), Validators.pattern, ForbiddenNameValidator(/admin/),]],
+      fName: ['', [Validators.required, Validators.minLength(3), Validators.pattern, ForbiddenNameValidator(/admin/),]],
 
       //Last Name Validator
-      lName: ['',[Validators.required, Validators.minLength(3), ForbiddenNameValidator(/admin/)]],
+      lName: ['', [Validators.required, Validators.minLength(3), ForbiddenNameValidator(/admin/)]],
 
-  
+
       //Email Validator
-      email: ['',[Validators.required, Validators.minLength(3), Validators.email]],
+      email: ['', [Validators.required, Validators.minLength(3), Validators.email]],
 
 
       //Password Validator
-      password: ['',[Validators.required, Validators.minLength(8)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
 
 
       //Confirm Password Validator
-      confirmPassword: ['',[Validators.required]],
+      confirmPassword: ['', [Validators.required]],
 
 
       //Contact Number Validator
-      contactNumber: ['',[Validators.required, ContactNumberValidator ]],
+      contactNumber: ['', [Validators.required, ContactNumberValidator]],
 
-    },{validator: PasswordValidator});
+    }, { validator: PasswordValidator });
   }
-  
-  //submit on click.
-  /*register(){
-    this.submitted = true;
-    if (this.registerForm.invalid) {
-      this.registerForm.markAsTouched(); //fields will remain marked once filled. Even after submit.
-      alert('Re-enter Correct Details.');
- 
 
-    }
-    else{
-      //this.registerForm.markAsTouched();
-      alert(JSON.stringify(this.user));
-      sessionStorage.setItem("registerDetails",JSON.stringify(this.user));
-      this.router.navigate(['/login']);
-      //alert('Details Has Been Registered.');
-     let resp=this.service.doRegistration(this.user);
-      resp.subscribe((data)=>this.message=data)
-      this.router.navigate(['/dashboardLink']);
-    }
-  }*/
- // search:User=new User();
- 
+  //submit on click.
   
-  info:String;
- 
-  register(){
+  // search:User=new User();
+
+
+  info: String;
+
+  register() {
     this.spinner.show();
     if (this.registerForm.invalid) {
       this.spinner.hide();
       this.registerForm.markAsTouched(); //fields will remain marked once filled. Even after submit.
       alert('Re-enter Correct Details.');
     }
-    
-    else{
+
+    else {
       this.spinner.hide();
-      alert(JSON.stringify(this.user));
+      //alert(JSON.stringify(this.user));
       //this.service.check(this.user).subscribe(data1=>{
-        //if(data1.status==false){
-          this.service.register(this.user).subscribe(data=>{
-            if(data.status==true){
-              console.log(data.statusMessage);
-              this.info=data.statusMessage;
-            }
-            else{
-              this.info=data.statusMessage;
-              alert(JSON.stringify(data.statusMessage));
-            }
-          })
-        //}
-        //else{
-          //alert("you have already registered please login");
-        //}
-      //})
-      
+      //if(data1.status==false){
+      this.service.register(this.user).subscribe(data => {
+        if (data.status == true) {
+          console.log(data.statusMessage);
+          this.info = data.statusMessage;
+        }
+        else {
+          this.info = data.statusMessage;
+          alert(JSON.stringify(data.statusMessage));
+        }
+      })
     }
     //this.router.navigate(['/login']);
   }
--- src/app/success-page/success-page.component.ts
+++ src/app/success-page/success-page.component.ts
@@ -2,8 +2,8 @@ import { ElementRef } from '@angular/core';
 import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import pdfMake from "pdfmake/build/pdfmake";  
-import pdfFonts from "pdfmake/build/vfs_fonts";  
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
@@ -12,13 +12,13 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
   styleUrls: ['./success-page.component.css']
 })
 export class SuccessPageComponent implements OnInit {
-  oneWayDetails: any={};
-  searchDetails: any={};
-  passengerDetail: any ={};
-  returnDetails: any ={};
-  seats : any;
-  flightClass : string;
-  scheduleId : string;
+  oneWayDetails: any = {};
+  searchDetails: any = {};
+  passengerDetail: any = {};
+  returnDetails: any = {};
+  seats: any;
+  flightClass: string;
+  scheduleId: string;
   customerId: number;
   noOfSeats: any;
   travelDate: any;
@@ -41,7 +41,7 @@ export class SuccessPageComponent implements OnInit {
   bookId: string;
   returnCityTo: any;
   returnCityFrom: any;
-  class="SinTicket";
+  class = "SinTicket";
   oneWayFare: string;
   returnFare: string;
   returnId: string;
@@ -52,38 +52,38 @@ export class SuccessPageComponent implements OnInit {
     this.oneWayDetails = JSON.parse(sessionStorage.getItem('oneWayDetails'));
     this.flightClass = this.oneWayDetails.classtype;
     this.scheduleId = this.oneWayDetails.scheduleid;
-    this.cityFrom=this.oneWayDetails.cityFrom;
-    this.cityTo=this.oneWayDetails.cityTo;
-    this.cityFromS=this.cityFrom.substring(0,3);
-    this.cityToS=this.cityTo.substring(0,3);
-    this.bookId= JSON.parse(sessionStorage.getItem('bookingId'));
+    this.cityFrom = this.oneWayDetails.cityFrom;
+    this.cityTo = this.oneWayDetails.cityTo;
+    this.cityFromS = this.cityFrom.substring(0, 3);
+    this.cityToS = this.cityTo.substring(0, 3);
+    this.bookId = JSON.parse(sessionStorage.getItem('bookingId'));
     this.customerName = (sessionStorage.getItem('userName'));
     this.customerId = Number(sessionStorage.getItem('customerId'));
-    this.oneWayFare= (sessionStorage.getItem('oneWayFare'));
-    this.returnFare= (sessionStorage.getItem('returnFare'));
-    this  .searchDetails = JSON.parse(sessionStorage.getItem('searchDetails'));
+    this.oneWayFare = (sessionStorage.getItem('oneWayFare'));
+    this.returnFare = (sessionStorage.getItem('returnFare'));
+    this.searchDetails = JSON.parse(sessionStorage.getItem('searchDetails'));
     this.noOfSeats = this.searchDetails.noOfPassengers;
     this.travelDate = this.searchDetails.travelDate;
-    this.seats=sessionStorage.getItem('seats');
-    this.seatSelected=this.seats;
-    this.returnId=(sessionStorage.getItem('returnId'));
-    
+    this.seats = sessionStorage.getItem('seats');
+    this.seatSelected = this.seats;
+    this.returnId = (sessionStorage.getItem('returnId'));
+
     if (sessionStorage.getItem('ReturnSearch') === 'null') {
       this.returnStatus = false;
       console.log('return not avail');
     } else {
       console.log('return avail');
-      this.class="TwoTickets";
+      this.class = "TwoTickets";
       this.returnDetails = JSON.parse(sessionStorage.getItem('returnDetails'));
       this.returnStatus = true;
       this.returnScheduleId = this.returnDetails.scheduleid;
       this.returnNoOfSeats = this.searchDetails.noOfPassengers;
       this.returnClass = this.returnDetails.classtype;
       this.returnTravelDate = this.searchDetails.returnTravelDate;
-      this.returnCityTo=this.returnDetails.cityTo;
-      this.returnCityFrom=this.returnDetails.cityFrom;
-      this.returnCityFromS = this.returnDetails.cityFrom.substring(0,3);
-      this.returnCityToS = this.returnDetails.cityTo.substring(0,3); 
+      this.returnCityTo = this.returnDetails.cityTo;
+      this.returnCityFrom = this.returnDetails.cityFrom;
+      this.returnCityFromS = this.returnDetails.cityFrom.substring(0, 3);
+      this.returnCityToS = this.returnDetails.cityTo.substring(0, 3);
     }
 
     this.passengerDetail = JSON.parse(
@@ -101,90 +101,84 @@ export class SuccessPageComponent implements OnInit {
     //console.log(this.);
   }
 
-  goHome()
-  {
+  goHome() {
     sessionStorage.clear();
     this.router.navigate(['/search']);
   }
-  savePdf() {    
-        
-    let docDefinition = {     
-        
-      header: 'Booking Details',      
+  savePdf() {
+
+    let docDefinition = {
+
+      header: 'Booking Details',
       content: [
         {
-          text:'Name :'+ this.customerName 
+          text: 'Name :' + this.customerName
         },
-      {
-        columns: [
-          [{
-            text: 'Booking Id : ' + this.bookId
-          },
-          {
-            text: 'Source : ' + this.cityFrom
-          },
-          {
-            text: 'Destination : ' + this.cityTo
-          },
-          {
-            text: 'Departure Time : ' + this.oneWayDetails.departureTime
-          },
-          {
-            text: 'Arrival Time : ' + this.oneWayDetails.arrivalTime
-          },
-          {
-            text: 'Seats Selected : ' + this.seats
-          },
-          {
-            text: 'Class : ' + this.flightClass
-          }] 
-         ]
-        }
-      ]          
-  }
-  pdfMake.createPdf(docDefinition).download();
-};
-    savePdf1() {    
-        
-      let docDefinition = {     
-        
-          header: 'Return Details',      
-          content: [
-            {
-              text:'Name :'+ this.customerName 
+        {
+          columns: [
+            [{
+              text: 'Booking Id : ' + this.bookId
             },
-          {
-            columns: [
-              [{
-                text: 'Return Id : ' + this.returnId
-              },
-              {
-                text: 'Source : ' + this.returnCityFrom
-              },
-              {
-                text: 'Destination : ' + this.returnCityTo
-              },
-              {
-                text: 'Departure Time : ' + this.returnDetails.departureTime
-              },
-              {
-                text: 'Arrival Time : ' + this.returnDetails.arrivalTime
-              },
-              /*{
-                text: 'Seats Selected : ' + this.seats
-              },*/
-              {
-                text: 'Class : ' + this.flightClass
-              }] 
-             ]
-            }
-          ]     
-        
-     
-       
-          
-            
-      }
-      pdfMake.createPdf(docDefinition).download();
-    };
+            {
+              text: 'Source : ' + this.cityFrom
+            },
+            {
+              text: 'Destination : ' + this.cityTo
+            },
+            {
+              text: 'Departure Time : ' + this.oneWayDetails.departureTime
+            },
+            {
+              text: 'Arrival Time : ' + this.oneWayDetails.arrivalTime
+            },
+            {
+              text: 'Seats Selected : ' + this.seats
+            },
+            {
+              text: 'Class : ' + this.flightClass
+            }]
+          ]
+        }
+      ]
+    }
+    pdfMake.createPdf(docDefinition).download();
+  };
+  savePdf1() {
+
+    let docDefinition = {
+
+      header: 'Return Details',
+      content: [
+        {
+          text: 'Name :' + this.customerName
+        },
+        {
+          columns: [
+            [{
+              text: 'Return Id : ' + this.returnId
+            },
+            {
+              text: 'Source : ' + this.returnCityFrom
+            },
+            {
+              text: 'Destination : ' + this.returnCityTo
+            },
+            {
+              text: 'Departure Time : ' + this.returnDetails.departureTime
+            },
+            {
+              text: 'Arrival Time : ' + this.returnDetails.arrivalTime
+            },
+            /*{
+              text: 'Seats Selected : ' + this.seats
+            },*/
+            {
+              text: 'Class : ' + this.flightClass
+            }]
+          ]
+        }
+      ]
+    }
+    pdfMake.createPdf(docDefinition).download();
+  };
 }
